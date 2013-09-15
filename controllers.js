dd03d2c0e2e82c56cb9348fee89d19fbb46602c1
James <git@jamedjo.co.uk>
2013-09-15 20:43:21 +0100

    Uses select dropdown. Extracted score calculation from update.

--- controllers.js
+++ controllers.js
@@ -3,18 +3,22 @@ app = angular.module("MakerScore",[]);
 app.directive('topicGroup', function(){
   return {
     controller: function($scope) {
-      var topics = $scope.topics = [];
+      $scope.topics = [];
       this.addTopic = function(topic) {
-        topics.push(topic);
+        $scope.topics.push(topic);
       };
-      $scope.score = 0;
-      $scope.completion = 0;
+      var calculateScore = function(topics){
+        var non_zero_topics = _.select(topics,function(t){return t.score!=0});
+        $scope.total = _.reduce(non_zero_topics,function(sum,t){return parseInt(t.score)+sum},0);
+        return { score: ($scope.total / non_zero_topics.length)||0,
+                completion: (non_zero_topics.length / topics.length)||0}
+      };
       this.updateScore = function(){
-        non_zero_topics = _.select(topics,function(t){return t.score!=0});
-        var total = _.reduce(non_zero_topics,function(sum,t){return t.score + sum},0);
-        $scope.score = total / non_zero_topics.length;
-        $scope.completion = non_zero_topics.length / topics.length;
+        var score_object = calculateScore($scope.topics);
+        $scope.score = score_object.score;
+        $scope.completion = score_object.completion;
       };
+      this.updateScore();
     },
     restrict: 'AE',
     template: '<div ng-transclude></div>',
@@ -30,7 +34,7 @@ app.directive('topic',function(){
     transclude: true,
     scope: {},
     require: '^topicGroup',
-    template: '<li class="topic"><span ng-transclude></span><input type="number" min="0" max="5" ng-model="score" ng-change="updateScore()"></li>',
+    template: '<li class="topic"><span ng-transclude></span><select ng-model="score" ng-change="updateScore()"><option value="0"></option><option value="1">Beginner</option><option value="2">Imitator</option><option value="3">Repeater</option><option value="4">Habitual</option><option value="5">Master</option></select></li>',
     link: function(scope, element, attrs,topicGroupController){
       topicGroupController.addTopic(scope);
       scope.score = 0;
