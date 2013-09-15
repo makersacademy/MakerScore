c213ee0a0f3b7c5f00cb5aff4cc2317761d1a0b7
James <git@jamedjo.co.uk>
2013-09-15 23:23:52 +0100

    Displays scores for subjects. Wrapped score in directive.

--- controllers.js
+++ controllers.js
@@ -4,31 +4,31 @@ app.directive('topicGroup', function(){
   return {
     controller: function($scope) {
       $scope.topics = [];
-      $scope.topics_by_subject = {};
-      $scope.scores_by_subject = {}
+      $scope.subject_topics = {};
+      $scope.subject_scores = {};
 
       this.addTopic = function(topic) {
         $scope.topics.push(topic);
-        if(typeof $scope.topics_by_subject[topic.subject]==='undefined'){
-          $scope.topics_by_subject[topic.subject]=[];
+        if(typeof $scope.subject_topics[topic.subject]==='undefined'){
+          $scope.subject_topics[topic.subject]=[];
         }
-        $scope.topics_by_subject[topic.subject].push(topic);
+        $scope.subject_topics[topic.subject].push(topic);
+        this.updateScore();
       };
       var calculateScore = function(topics){
         var non_zero_topics = _.select(topics,function(t){return t.score!=0});
-        $scope.total = _.reduce(non_zero_topics,function(sum,t){return parseInt(t.score)+sum},0);
-        return { score: ($scope.total / non_zero_topics.length)||0,
+        var total = _.reduce(non_zero_topics,function(sum,t){return parseInt(t.score)+sum},0);
+        return { score: (total / non_zero_topics.length)||0,
                 completion: (non_zero_topics.length / topics.length)||0}
       };
       this.updateScore = function(){
-        _.each($scope.topics_by_subject,function(topics,subject){
-          $scope.scores_by_subject[subject] = calculateScore(topics);
+        _.each($scope.subject_topics,function(topics,subject){
+          $scope.subject_scores[subject] = calculateScore(topics);
         });
         var overall_scores = calculateScore($scope.topics);
         $scope.score = overall_scores.score;
         $scope.completion = overall_scores.completion;
       };
-      this.updateScore();
     },
     restrict: 'AE',
     template: '<div ng-transclude></div>',
@@ -47,11 +47,19 @@ app.directive('topic',function(){
     template: '<li class="topic"><span ng-transclude></span><select ng-model="score" ng-change="updateScore()"><option value="0"></option><option value="1">Beginner</option><option value="2">Imitator</option><option value="3">Repeater</option><option value="4">Habitual</option><option value="5">Master</option></select></li>',
     link: function(scope, element, attrs,topicGroupController){
       scope.subject = attrs.subject;
-      topicGroupController.addTopic(scope);
       scope.score = 0;
+      topicGroupController.addTopic(scope);
       scope.updateScore = function() {
         topicGroupController.updateScore();
       };
     }
+  };
+});
+
+app.directive('score', function(){
+  return {
+    scope: {score:"@",completion:"@"},
+    restrict: 'E',
+    template: '<span class="score"><span>JSA: {{score | number:1}}</span> | <span>Completion: {{completion * 100 | number:0 }}%</span></span>'
   };
 });
